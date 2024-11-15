2eeb1f809f90528768fb0b55168c8cb8790ca8bf
lozovoiPA <pa.lozovoi.mail@gmail.com>
2024-11-15 16:21:27 +0700

    lab1

---
+++ Lab1/App.java
@@ -0,0 +1,84 @@
+package Lab1;
+
+public class App{
+    public static boolean ready = false;
+    
+    public static void main(String[] args){
+        Product product = new Product();
+        Provider provider = new Provider(product);
+        Consumer consumer = new Consumer(product);
+        new Thread(provider).start();
+        new Thread(consumer).start();
+    }
+}
+
+class Product{
+    public synchronized void provide(){
+        if (App.ready)
+            return;
+        App.ready = true;
+        notify();
+    }
+    public synchronized void consume(){
+        while(!App.ready){
+            try{
+                wait();
+            }
+            catch(InterruptedException e){ // без этого выдает ошибку. Видимо, обязательно необходима обработка случая прерывания потока (исключения e).
+            }
+        }
+        App.ready = false;
+    }
+    public boolean shown = true;
+}
+
+class Provider implements Runnable{
+    private Product product;
+    private boolean doStop = false;
+
+    public Provider(Product product){
+        this.product = product;
+    }
+    public synchronized void doStop(){
+        doStop = true;
+    }
+    private synchronized boolean keepRunning(){
+        return doStop == false;
+    }
+
+    private void event(){
+        product.provide();
+    }
+    
+    public void run(){
+        while(keepRunning()){
+            try{
+                Thread.sleep(1L * 1000L);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("Инициация события!");
+            event();
+        }
+    }
+}
+class Consumer implements Runnable{
+    private Product product;
+    private boolean doStop = false;
+
+    public Consumer(Product product){
+        this.product = product;
+    }
+    public synchronized void doStop(){
+        doStop = true;
+    }
+    private synchronized boolean keepRunning(){
+        return doStop == false;
+    }
+
+    public void run(){
+        while(keepRunning()){
+            product.consume();
+            System.out.println("Принял событие!");
+        }
+    }
+}
